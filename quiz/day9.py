4fca9a3cc59a037c3562796849af1c4dfbd5fc47
jisu <jisu9329@gmail.com>
2022-08-29 00:20:24 +0900

    #28 ~ #30
    30문제까지 완료!

---
+++ quiz/day9.py
@@ -0,0 +1,22 @@
+#28 2-gram
+
+# text = input()
+
+# for i in range(len(text)-1) :
+#     print(text[i], text[i + 1], sep='')
+
+#29 ㄷㅐ문자 검증기
+
+# upper_name = input()
+
+# if upper_name.isupper :
+#     print("YES")
+# else : 
+#     print("NO")
+
+#30 
+
+sentence = input()
+word = input()
+
+print(sentence.find(word))
