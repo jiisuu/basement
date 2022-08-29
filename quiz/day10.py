f640728b8f8d14d25b8a37ff2f8854c38a1146df
jisu <jisu9329@gmail.com>
2022-08-29 23:28:09 +0900

    #31 #32 | split() , strip()
    두 메서드 이해하기!

---
+++ quiz/day10.py
@@ -0,0 +1,23 @@
+#32 문자열을 입력받으면 단어의 갯수를 출력하는 프로그램
+sentence = input()
+l1 = sentence.split().strip()
+print(len(l1))
+print(type(l1))
+
+sentence = input()
+l2 = sentence.strip().split()
+print(len(l2)) 
+print(type(l2))
+
+# n = input()
+# l = list(n.strip().split())
+# print(len(l)) ##test1 test2 tes3 ₩
+
+test = "test".split("s")
+print(test)
+
+test2 = "test test2".strip()
+test3 = "   test   test2   ".strip()
+
+print(test2)
+print(test3)
