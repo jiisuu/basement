1dd2b480312ac1445fb840315877b686190733e2
jisu <jisu9329@gmail.com>
2022-09-09 21:52:12 +0900

    #35, #36

---
+++ quiz/day13.py
@@ -0,0 +1,22 @@
+#2제곱, 3제곱, 4제곱을 할 수 있는 Factory 함수를 만들려고 합니다
+# def one(n):
+#     def two(l):
+#         return l**n
+#     return two
+
+# a = one(2)
+# b = one(3)
+# c = one(4)
+
+# print(a(10))
+# print(b(10))
+# print(c(10))
+
+#36 구구단 만들기
+# a = int(input())
+# for i in range(1, 10):
+#     print(a * i)
+
+#37
+a = input('투표 결과를 입력하세요.')
+print(a.count('혜원'))
